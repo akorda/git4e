26b3584f93dca67b3e11d82af0cced8b78b0f29f
Antonis Kordatzakis <akorda@gmail.com>
2021-05-07 15:26:08 +0300

    Add Sqlite Chinook models
    Downloaded from here: https://www.sqlitetutorial.net/sqlite-sample-database/

---
+++ src/Chinook/Artist.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Git4e;
+using ProtoBuf;
+
+namespace Chinook
+{
+    public class Artist : HashableObject
+    {
+        public const string ArtistContentType = "Artist";
+
+        [ProtoContract]
+        public class ArtistContent : IContent
+        {
+            [ProtoMember(1)]
+            public int ArtistId { get; set; }
+            [ProtoMember(2)]
+            public string Name { get; set; }
+            [ProtoMember(3)]
+            public string[] AlbumFullHashes { get; set; }
+
+            public Task<IHashableObject> ToHashableObjectAsync(string hash, IServiceProvider serviceProvider, CancellationToken cancellationToken = default)
+            {
+                var albums =
+                    this.AlbumFullHashes
+                    .Select(posHash => new LazyAlbum(posHash))
+                    .ToList();
+                var artist = new Artist(hash)
+                {
+                    ArtistId = this.ArtistId,
+                    Name = this.Name,
+                    Albums = albums
+                };
+                return Task.FromResult(artist as IHashableObject);
+            }
+        }
+
+        int _ArtistId;
+        public int ArtistId
+        {
+            get => _ArtistId;
+            set
+            {
+                if (_ArtistId != value)
+                {
+                    _ArtistId = value;
+                    this.MarkAsDirty();
+                }
+            }
+        }
+
+        string _Name;
+        public string Name
+        {
+            get => _Name;
+            set
+            {
+                if (_Name != value)
+                {
+                    _Name = value;
+                    this.MarkAsDirty();
+                }
+            }
+        }
+
+        List<LazyAlbum> _Albums;
+        public List<LazyAlbum> Albums
+        {
+            get => _Albums;
+            set
+            {
+                if (_Albums != value)
+                {
+                    _Albums = value;
+                    this.MarkAsDirty();
+                }
+            }
+        }
+
+        public Artist()
+            : this(null)
+        {
+        }
+
+        public Artist(string hash = null)
+            : base(ArtistContentType, hash)
+        {
+        }
+
+        protected override object GetContent()
+        {
+            var albumFullHashes = this.Albums?
+                .OrderBy(album => album.HashIncludeProperty1)
+                .Select(album => album.FullHash)
+                .ToArray();
+            var content = new ArtistContent
+            {
+                ArtistId = this.ArtistId,
+                Name = this.Name,
+                AlbumFullHashes = albumFullHashes
+            };
+            return content;
+        }
+
+        public override async IAsyncEnumerable<IHashableObject> GetChildObjects()
+        {
+            var albums = this.Albums ?? new List<LazyAlbum>();
+            foreach (var album in albums)
+            {
+                yield return await Task.FromResult(album);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Artist Hash with the following included properties:
+    /// 1. ArtistId
+    /// 2. Artist Name
+    /// </summary>
+    public class LazyArtist : LazyHashableObject<int, string>
+    {
+        public LazyArtist(string fullHash)
+            : base(fullHash, Artist.ArtistContentType)
+        {
+        }
+
+        public LazyArtist(Artist artist)
+            : base(artist, a => (a as Artist).ArtistId, a => (a as Artist).Name)
+        {
+        }
+    }
+}
---
+++ src/Chinook/Track.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Git4e;
+using ProtoBuf;
+
+namespace Chinook
+{
+    public class Track : HashableObject
+    {
+        public const string TrackContentType = "Track";
+
+        [ProtoContract]
+        public class TrackContent : IContent
+        {
+            [ProtoMember(1)]
+            public int TrackId { get; set; }
+            [ProtoMember(2)]
+            public string Name { get; set; }
+            [ProtoMember(3)]
+            public string Composer { get; set; }
+            [ProtoMember(4)]
+            public int Milliseconds { get; set; }
+            [ProtoMember(5)]
+            public int Bytes { get; set; }
+            [ProtoMember(6)]
+            public decimal UnitPrice { get; set; }
+            [ProtoMember(7)]
+            public string MediaTypeFullHash { get; set; }
+            [ProtoMember(8)]
+            public string GenreFullHash { get; set; }
+
+            public Task<IHashableObject> ToHashableObjectAsync(string hash, IServiceProvider serviceProvider, CancellationToken cancellationToken = default)
+            {
+                var track = new Track(hash)
+                {
+                    TrackId = this.TrackId,
+                    Name = this.Name,
+                    Composer = this.Composer,
+                    Milliseconds = this.Milliseconds,
+                    Bytes = this.Bytes,
+                    UnitPrice = this.UnitPrice,
+                    MediaType = new LazyHashableObject(this.MediaTypeFullHash, Chinook.MediaType.MediaTypeContentType),
+                    Genre = new LazyHashableObject(this.GenreFullHash, Chinook.Genre.GenreContentType)
+                };
+                return Task.FromResult(track as IHashableObject);
+            }
+        }
+
+        int _TrackId;
+        public int TrackId
+        {
+            get => _TrackId;
+            set
+            {
+                if (_TrackId != value)
+                {
+                    _TrackId = value;
+                    this.MarkAsDirty();
+                }
+            }
+        }
+
+        string _Name;
+        public string Name
+        {
+            get => _Name;
+            set
+            {
+                if (_Name != value)
+                {
+                    _Name = value;
+                    this.MarkAsDirty();
+                }
+            }
+        }
+
+        string _Composer;
+        public string Composer
+        {
+            get => _Composer;
+            set
+            {
+                if (_Composer != value)
+                {
+                    _Composer = value;
+                    this.MarkAsDirty();
+                }
+            }
+        }
+
+        int _Milliseconds;
+        public int Milliseconds
+        {
+            get => _Milliseconds;
+            set
+            {
+                if (_Milliseconds != value)
+                {
+                    _Milliseconds = value;
+                    this.MarkAsDirty();
+                }
+            }
+        }
+
+        int _Bytes;
+        public int Bytes
+        {
+            get => _Bytes;
+            set
+            {
+                if (_Bytes != value)
+                {
+                    _Bytes = value;
+                    this.MarkAsDirty();
+                }
+            }
+        }
+
+        decimal _UnitPrice;
+        public decimal UnitPrice
+        {
+            get => _UnitPrice;
+            set
+            {
+                if (_UnitPrice != value)
+                {
+                    _UnitPrice = value;
+                    this.MarkAsDirty();
+                }
+            }
+        }
+
+        //in memory only
+        internal int AlbumId { get; set; }
+
+        //do not serialize
+        internal string GenreId { get; set; }
+
+        public LazyHashableObject Genre { get; set; }
+
+        //do not serialize
+        internal string MediaTypeId { get; set; }
+
+        public LazyHashableObject MediaType { get; set; }
+
+        public Track()
+            : this(null)
+        {
+        }
+
+        public Track(string hash = null)
+            : base(TrackContentType, hash)
+        {
+        }
+
+        protected override object GetContent()
+        {
+            var content = new TrackContent
+            {
+                TrackId = this.TrackId,
+                Name = this.Name,
+                Composer = this.Composer,
+                Milliseconds = this.Milliseconds,
+                Bytes = this.Bytes,
+                UnitPrice = this.UnitPrice,
+                MediaTypeFullHash = this.MediaType?.FullHash,
+                GenreFullHash = this.Genre?.FullHash
+            };
+            return content;
+        }
+
+        public override async IAsyncEnumerable<IHashableObject> GetChildObjects()
+        {
+            if (this.Genre != null)
+            {
+                yield return await Task.FromResult(this.Genre);
+            }
+            if (this.MediaType != null)
+            {
+                yield return await Task.FromResult(this.MediaType);
+            }
+        }
+    }
+
+    /// <summary>
+    /// VesselPosition Hash with the following included properties:
+    /// 1. TrackId
+    /// 2. Genre->GenreId
+    /// 3. MediaType->MediaTypeId
+    /// </summary>
+    public class LazyTrack : LazyHashableObject<int, string, string>
+    {
+        public LazyTrack(string fullHash)
+            : base(fullHash, Track.TrackContentType)
+        {
+        }
+
+        public LazyTrack(Track track)
+            : base(track, t => (t as Track).TrackId, t => (t as Track).Genre?.GetValue<Genre>().GenreId, t => (t as Track).MediaType?.GetValue<MediaType>().MediaTypeId)
+        {
+        }
+    }
+}
