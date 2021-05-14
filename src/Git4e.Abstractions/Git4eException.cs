9090ece061c04405bbea17eda994a4af12aca278
Antonis Kordatzakis <akorda@gmail.com>
2021-05-14 08:09:48 +0300

    Add custom Git4eException class
    The Git4e library must throw this type of exceptions or a derived one

---
+++ src/Git4e.Abstractions/Git4eException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Git4e
+{
+    public class Git4eException : Exception
+    {
+        public Git4eException()
+        {
+        }
+
+        public Git4eException(string message)
+            : base(message)
+        {
+        }
+
+        public Git4eException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
