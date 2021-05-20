e8f1d15ed93309c00f38ddde34252d47c98a91fc
Antonis Kordatzakis <akorda@gmail.com>
2021-05-20 21:24:08 +0300

    Add custom git4e Attributes
    These attributes will mark properties/collections
    - that are serialized in content
    - could be used for automatic comparison e.g. in CommitComparer
    
    #10

---
+++ src/Git4e/ContentCollectionAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Git4e
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class ContentCollectionAttribute : Attribute
+    {
+    }
+}
